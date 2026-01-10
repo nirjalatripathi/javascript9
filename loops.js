e172751f734d3198b2b474b24cce78d0d683d7c8
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-10 22:16:30 +0545

    [ADD]: Star patter2

--- loops.js
+++ loops.js
@@ -60,3 +60,12 @@ for (let i=1; i<=5; i++) {
     }
     console.log (row);
 }
+
+//Q.6. Use nested loops to print the following pattern.
+for (let i = 1; i <=5; i++) {
+    let row=" ";
+    for ( let j = 1; j <=i; j++) {
+        row += "*";
+    }
+    console.log (row);
+}
