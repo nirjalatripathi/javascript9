33d738a867cb1964f7a07630aadcd07423c2f657
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-11 13:19:11 +0545

    [ADD]: Area of rectangle

--- functions.js
+++ functions.js
@@ -1,7 +1,15 @@
 // TOPIC 2: FUNCTIONS 
-// A function to greetuser
+//Q.1. A function to greetuser
 function greetUser (name) {
     return "Hello " + name + "! Welcome to JavaScript.";
 
 }
 console.log (greetUser (" Nirjala"));
+
+//Q.2. Function to calculate area of rectangle 
+function calculateArea (length, width) {
+let area= length* width;
+return area;
+}
+let result= calculateArea (5,8);
+console.log ("Area of rectangle is",result);
