894b4d12b63298a02018bf26564222ed60c6119b
nirjalatripathi <nirjalatripathi41@gmail.com>
2026-01-11 13:30:46 +0545

    [ADD]: Maximun number in array

--- functions.js
+++ functions.js
@@ -26,4 +26,16 @@ function celsiusToFahrenheit (celsius) {
 }
 console.log ("5 degree C in Fahrenheit is", celsiusToFahrenheit(5));
 
+// Q.5. Find maximum in array 
+function findMax (numbers) {
+    let max= numbers[0];
+    for (let i=1; i<numbers.length; i++) {
+        if (numbers[i] > max) {
+            max=numbers[i];
+        }
+    }
+    return max;
+}
+console.log ("maximum value:", findMax([3, 7, 2, 9, 1]));
+
 
